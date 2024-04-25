ce2fbe949a57030c22c2bccd266a0e4586ad1ad7
rozannouval <rozannouval@gmail.com>
2024-04-25 19:58:52 +0700

    memperbarui navbar dan menambah about

--- src/js/index.js
+++ src/js/index.js
@@ -9,23 +9,5 @@ let typedTitle = new Typed(".typing-title", {
   loop: true,
 });
 
-let lastScrollTop = 0;
-
-window.addEventListener(
-  "scroll",
-  function () {
-    let currentScroll =
-      window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > lastScrollTop) {
-      // Scroll down
-      document.getElementById("navbar").style.top = "-60px"; // Hide navbar
-    } else {
-      // Scroll up
-      document.getElementById("navbar").style.top = "0"; // Show navbar
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
-  },
-  false
-);
+// Toggle class active untuk hamburger menu
+const navbarNavigasi = document.querySelector(".text-hilang");
